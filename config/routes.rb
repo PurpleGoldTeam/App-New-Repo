179df8e88011a5a7a5d4fbbb23323ac9151917ca
Ruffine DetohMessan <ruffine.detoh-messan@mnsu.edu>
2018-03-27 23:29:33 +0000

    First Push

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :users
+  get 'static_pages/home'
+
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+
+  
+  
+ 
+  #root 'users#index'
+  root 'static_pages#home'
+end
