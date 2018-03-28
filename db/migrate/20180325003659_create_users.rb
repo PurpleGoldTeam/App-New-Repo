09997a7ede77480e56677fbd766232a5e90426ba
Bridget Yokiel <bridget.yokiel@mnsu.edu>
2018-03-28 13:36:06 +0000

    adding fields to database

--- db/migrate/20180325003659_create_users.rb
+++ db/migrate/20180325003659_create_users.rb
@@ -3,6 +3,22 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :bio
+      t.string :race
+      t.string :sex
+      t.string :religion
+      t.string :education
+      t.string :ocupation
+      t.string :income
+      t.string :smoker
+      t.string :height
+      t.string :weight
+      t.string :age
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+      
 
       t.timestamps
     end
