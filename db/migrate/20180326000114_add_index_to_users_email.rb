4b0734eac2fc1353062b2e3f0e350681e39718e9
Bridget Yokiel <bridget.yokiel@mnsu.edu>
2018-03-31 03:17:02 +0000

    Adding the gender buttons and some headers

--- db/migrate/20180326000114_add_index_to_users_email.rb
+++ db/migrate/20180326000114_add_index_to_users_email.rb
@@ -1,5 +1,18 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[5.1]
   def change
-    add_index :users, :email, unique: true
+    add_index :users, :email, :bio ,:race
+      ,:sex
+      ,:religion
+      ,:education
+      ,:ocupation
+      ,:income
+      ,:smoker
+      ,:height
+      ,:weight
+      ,:age
+      ,:address
+      ,:city
+      ,:state
+      ,:zipunique: true
   end
 end
