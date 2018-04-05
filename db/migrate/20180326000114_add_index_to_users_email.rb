3c8db9d19fefaf2a3f4bf73263e852f2743bce3c
Bridget Yokiel <bridget.yokiel@mnsu.edu>
2018-04-05 22:12:12 +0000

    Sign up html gender buttons

--- db/migrate/20180326000114_add_index_to_users_email.rb
+++ db/migrate/20180326000114_add_index_to_users_email.rb
@@ -1,14 +1,7 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[5.1]
   def change
-    add_index :users, :email, :bio ,:race
-      ,:sex
-      ,:religion
-      ,:education
-      ,:ocupation
-      ,:income
-      ,:smoker
-      ,:height
-      ,:weight
+    add_index :users, :email, :bio ,:race ,:sex ,:religion ,:education ,
+    :ocupation ,:income ,:smoker ,:height ,:weight
       ,:age
       ,:address
       ,:city
