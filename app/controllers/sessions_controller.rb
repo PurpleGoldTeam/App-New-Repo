617897d334b05eda54da28aabdd1ad811ab90eff
Jacques Castonguay <jacques.castonguay@mnsu.edu>
2018-04-06 22:58:39 +0000

    no changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,18 @@ class SessionsController < ApplicationController
     #user shouldn't have to be logged in before logging in
     skip_before_action :set_current_user
     def create
-        auth=request.env["omniauth.auth"]
-        user=Ssouser.where(:provider => auth["provider"], :uid => auth["uid"]).first ||
-        Ssouser.create_with_omniauth(auth)
-        session[:user_id] = user.id
-        redirect_to root_path
+        #if request.fullpath == '/signup' || request.fullpath == '/login'
+         #   redirect_to '/help'
+          #  #ruffine's create
+        #else
+        
+        
+            auth=request.env["omniauth.auth"]
+            user=Ssouser.where(:provider => auth["provider"], :uid => auth["uid"]).first ||
+            Ssouser.create_with_omniauth(auth)
+            session[:user_id] = user.id
+            redirect_to root_path
+        #end
     end
     def destroy
         session.delete(:user_id)
