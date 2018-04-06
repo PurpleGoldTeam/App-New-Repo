17595bfa4864879a02ef2b1f303c7fc8499c8dcc
Jacques Castonguay <jacques.castonguay@mnsu.edu>
2018-04-06 23:31:58 +0000

    renames sso's session create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,12 @@
 class SessionsController < ApplicationController
     #user shouldn't have to be logged in before logging in
     skip_before_action :set_current_user
-    def create
-        #if request.fullpath == '/signup' || request.fullpath == '/login'
-         #   redirect_to '/help'
-          #  #ruffine's create
-        #else
-        
-        
-            auth=request.env["omniauth.auth"]
-            user=Ssouser.where(:provider => auth["provider"], :uid => auth["uid"]).first ||
-            Ssouser.create_with_omniauth(auth)
-            session[:user_id] = user.id
-            redirect_to root_path
-        #end
+    def sso_create
+        auth=request.env["omniauth.auth"]
+        user=Ssouser.where(:provider => auth["provider"], :uid => auth["uid"]).first ||
+        Ssouser.create_with_omniauth(auth)
+        session[:user_id] = user.id
+        redirect_to root_path
     end
     def destroy
         session.delete(:user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/users',   to: 'users#create'
   #SSO routes
-  get 'auth/:provider/callback' => 'sessions#create'
+  get 'auth/:provider/callback' => 'sessions#sso_create'
   post '/logout' => 'sessions#destroy'
   get 'auth/failure' => 'sessions#failure'
   get 'auth/twitter', :as => 'login'
