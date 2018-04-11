88ef27aed0a21c6058e7795b7d4c0b16c1b91688
Jacques Castonguay <jacques.castonguay@mnsu.edu>
2018-04-11 19:25:01 +0000

    adds redirection to user when logged in

--- db/migrate/20180407210440_create_profiles.rb
+++ db/migrate/20180407210440_create_profiles.rb
@@ -1,6 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration[5.1]
   def change
     create_table :profiles do |t|
+      t.references :owner
       t.string :name
       t.string :email
       t.string :bio
