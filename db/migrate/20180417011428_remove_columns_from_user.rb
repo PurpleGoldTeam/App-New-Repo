0140b59fd6d715cb826266d5857f974c9de219e9
Jacques Castonguay <jacques.castonguay@mnsu.edu>
2018-04-17 01:35:20 +0000

    Delete comment

--- db/migrate/20180417011428_remove_columns_from_user.rb
+++ db/migrate/20180417011428_remove_columns_from_user.rb
@@ -17,24 +17,6 @@ class RemoveColumnsFromUser < ActiveRecord::Migration[5.1]
     remove_column :users, :zip
     
     
-=begin
-      t.string :name
-      t.string :email
-      t.string :bio
-      t.string :race
-      t.string :sex
-      t.string :religion
-      t.string :education
-      t.string :ocupation
-      t.string :income
-      t.string :smoker
-      t.string :height
-      t.string :weight
-      t.string :age
-      t.string :address
-      t.string :city
-      t.string :state
-      t.string :zip
-=end
+
   end
 end
